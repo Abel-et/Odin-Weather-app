930b3f8c6058a8fb27eaf4e09cd08d3d592a4ca0
Abel-et <at6796629@gmail.com>
2026-02-17 16:29:46 +0300

    ui modification

--- weather.js
+++ weather.js
@@ -1,28 +1,108 @@
-console.log('Everything is ready')
 
-const user_location = "London"
-const form = document.querySelector('form')
-const input = document.getElementById('location')
 const api = '2G74XVA757XKXN4KZZGDVPE7C'
-fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(user_location)},CA?unitGroup=metric&key=2G74XVA757XKXN4KZZGDVPE7C&contentType=json`).then(response =>{
+const form = document.querySelector('form')
+const city_name = document.getElementById('city-name')
+let place = null
+let user_location = ''
+
+
+// Entry of functionality 
+form.addEventListener('submit',async (event)=>{
+  event.preventDefault()
+
+  // getting the country name from user
+  user_location = document.getElementById('location').value
+  place = await getData(user_location)
+  head_location(place)
+  description(place)
+  weeklyForecast(place)
+  console.log(place)
+  
+
+
+  // send the user location to get data 
+  
+})
+
+async function getData(user_location){
+  const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(user_location)},CA?unitGroup=metric&key=2G74XVA757XKXN4KZZGDVPE7C&contentType=json`);
+
   if(!response){
     throw new Error('Invalid location ')
   }
-  return response.json()
-}).then(data=>{
-  console.log('weather data :',data)
-}).catch(error =>{
-  console.log('Error: '+error.message)
-})
+
+  const data = await response.json()
+  return data
+}
+
+
+
+function head_location(place){
+ const city_name = document.getElementById('city-name')
+ const weather_icon = document.querySelector('#current_condition')
+
+//  access place object and pass the location name and icon
+city_name.textContent = place.resolvedAddress
+weather_icon.src = `./asset/4th Set - Color/${encodeURIComponent(place.currentConditions.icon)}.svg`
+}
+
+// display the weather discription and another infos
+
+function description(place){
+  const discrpt = document.querySelector('#discription')
+  const longitude = document.querySelector('#long')
+  const latitude = document.querySelector('#lang')
+  const rise = document.querySelector('#rise')
+  const  set = document.querySelector('#set')
+
+  latitude.textContent = "Latitude :"+ place.latitude
+  longitude.textContent = "Longitude :"+ place.longitude
+  rise.textContent = "Sunrise :"+ place.currentConditions.sunrise
+  set.textContent = "Sunset :" + place.currentConditions.sunrise
+  discrpt.textContent = place.description
+
+}
+
+
+
+// creating weekly forecast un order list
+
+function weeklyForecast(place){
+  const forecast = document.querySelector('#forecasts')
+  // putting an ai data to the ul list 
+  // datetime
+  forecast.innerHTML = ''
+  for(let i = 0 ; i<15; i++){
+    const dayName = new Date(place.days[i].datetime).toLocaleDateString('en-US', { weekday: 'long' })
+    const ul = document.createElement('ul')
+    ul.classList.add('day')
+
+    const head = document.createElement('div')
+    head.classList.add('head')
+
+    const span = document.createElement('span')
+    span.textContent = dayName
+    
+    const h3 = document.createElement('h3')
+    h3.textContent = place.days[i].conditions
+
+    head.appendChild(span) ;head.appendChild(h3)
+
+    const mid_section = document.createElement('div')
+    const img = document.createElement('img')
+    img.src = `./asset/4th Set - Color/${encodeURIComponent(place.days[i].icon)}.svg`
+    mid_section.appendChild(img)
+
+    const foot = document.createElement('div')
+    const span2 = document.createElement('span')
+    span2.textContent = "Temp: "+ place.days[i].temp
+    foot.appendChild(span2)
+
+    ul.appendChild(head); ul.appendChild(mid_section);ul.appendChild(foot)
+    forecast.appendChild(ul)
+  }
+}
 
 
 
 
-/*
-  Information i can gathered
-    1 current condition 
-    2 15 days weather forecast
-    3 temp , sun rise/set , humidity , wind speed 
-    4 hourly forecast and weekly forecast 
-
-*/
